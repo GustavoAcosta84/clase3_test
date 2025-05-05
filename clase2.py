209b274c5da6551a360f899e5919e1648b546022
GustavoAcosta84 <acgtavo@gmail.com>
2025-05-05 20:28:26 -0300

    segundo commit

---
+++ clase2.py
@@ -0,0 +1,53 @@
+# int_1 = 5
+# int_2 = 2
+
+# # suma
+# suma = int_1 + int_2
+# print(f"La suma entre {int_1} y {int_2} es: {suma}")
+
+# # resta
+# resta = int_1 - int_2
+# print(f"La resta entre {int_1} y {int_2} es: {resta}")
+
+# # multiplicacion
+# multiplicacion = int_1 * int_2
+# print(f"La multiplicacion entre {int_1} y {int_2} es: {multiplicacion}")
+
+# # division
+# division = int_1 / int_2
+# print(f"La division entre {int_1} y {int_2} es: {division}")
+
+# # potencia
+# potencia = int_1 ** int_2
+# print(f"La potencia entre {int_1} y {int_2} es: {potencia}")
+
+
+# # variable string
+# str_1 = "info"
+# str_2 = "rmatorio"
+
+# # operadores
+# # suma
+# suma = str_1 + str_2
+# print(f"La suma entre {str_1} y {str_2} es: {suma}")
+
+# # variables listas
+# list_1 = [1, 2, 3]
+# list_2 = [4, 5, 6]
+
+# # operadores 
+# # suma
+# suma = lis_1 + lis_2
+# print(f"La suma entre {list_1} y {list_2} es: {suma}"
+      
+anio_actual = 2025
+mayoria_edad = 18
+
+anio_nac = int(input("Ingresar anio de nacimiento: "))
+anio_usuario = anio_actual - anio_nac
+
+if (anio_usuario >= mayoria_edad):
+    print("El usuario es mayor de edad...:)")
+else:
+    print("El usuario No es mayor de edad...:(")
+
---
+++ clase2_Operaciones.py
@@ -0,0 +1,58 @@
+# ## Variables Enteras
+# int_1 = 5
+# int_2 = 2
+
+# ## Operadores
+# # # suma
+# suma = int_1 + int_2
+# print(f"La suma entre {int_1} y {int_2} es: {suma}")
+
+# # # resta
+# resta = int_1 - int_2
+# print(f"La resta entre {int_1} y {int_2} es: {resta}")
+
+# # # multiplicacion
+# multiplicacion = int_1 * int_2
+# print(f"La multiplicacion entre {int_1} y {int_2} es: {multiplicacion}")
+
+# # # division
+# division = int_1 / int_2
+# print(f"La division entre {int_1} y {int_2} es: {division}")
+
+# # # potencia
+# potencia = int_1 ** int_2
+# print(f"La potencia entre {int_1} y {int_2} es: {potencia}")
+
+
+# ## Variables String
+# str_1 = "info"
+# str_2 = "rmatorio"
+
+# # # operadores
+# # # suma
+# suma = str_1 + str_2
+# print(f"La suma entre {str_1} y {str_2} es: {suma}")
+
+# # variables listas
+# list_1 = [1, 2, 3]
+# list_2 = [4, 5, 6]
+
+# # operadores 
+# # suma
+# suma = list_1 + list_2
+# print(f"La suma entre {list_1} y {list_2} es: {suma}")
+
+## resta NO SE PUEDE RESTAR STRING
+# resta = list_1 - list_2
+# print(f"La resta entre {list_1} y {list_2} es: {resta}")
+
+## PARA ORDENAR UNA LISTA
+# lista = [93,12383,203,21982,38747.23,123,23,11,1,25,4]
+# # utilizando .sort()
+# lista.sort()
+# print(lista)
+
+## Para seleccionar un elemento de una lista
+## Para seleccionar el ultimo elemento se coloca [-1]
+# lista = [10,20,30,40,50]
+# print("El elemento seleccionado es: ",lista[2])
